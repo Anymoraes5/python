94b5355420b66641f9f24c8915f426abc826f88f
Ana Caroline Moraes <166078471+Anymoraes5@users.noreply.github.com>
2025-02-11 20:49:47 -0300

    [feat] Aula 01 - desafios simples

---
+++ desafio1.py
@@ -0,0 +1,14 @@
+print('==============DESAFIO 1 ============')
+nome = input('Qual o  seu nome? ')
+print('Olá', nome, 'Prazer em te conhecer!')
+
+print ('=============DESAFIO 2 ============')
+dia= input ('Qual dia você nasceu? ')
+mes=input('Qual mês vocês nasceu? ')
+ano=input('Qual ano você nasceu? ')
+print('Você nasceu em  ', dia, '/ ', mes, '/ ', ano)
+
+print('==============DESAFIO 3 ============')
+n1=input('Primeiro número: ')
+n2=input('Segundo número: ')
+print ('A soma é', n1+n2)
