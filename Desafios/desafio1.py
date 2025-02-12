0fdb25a5de1036ab07f34fe6489e7d975c2561e5
Ana Caroline Moraes <166078471+Anymoraes5@users.noreply.github.com>
2025-02-12 18:36:06 -0300

    [fix] organização

---
+++ Desafios/desafio1.py
@@ -0,0 +1,16 @@
+print('==============DESAFIO 1 ============')
+nome = input('Qual o  seu nome? ')
+print('Olá', nome, 'Prazer em te conhecer!')
+
+print ('=============DESAFIO 2 ============')
+dia= input ('Qual dia você nasceu? ')
+mes=input('Qual mês vocês nasceu? ')
+ano=input('Qual ano você nasceu? ')
+print('Você nasceu em  ', dia, '/ ', mes, '/ ', ano)
+
+print('==============DESAFIO 3 ============')
+n1=int(input('Primeiro número: '))
+n2=int(input('Segundo número: '))
+s= n1+n2
+print ('A soma é', s)
+print('Asoma vale {}'.format(s))
