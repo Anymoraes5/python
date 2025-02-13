86da376c50ee66d1900292da42136281f615daca
Ana Caroline Moraes <166078471+Anymoraes5@users.noreply.github.com>
2025-02-13 19:35:03 -0300

    [FIX] consertando pequenas coisas

--- Desafios/ex_009.py
+++ Desafios/ex_009.py
@@ -1,4 +1,6 @@
 #Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada
 t = int(input('Digite um número: '))
+print('-'*12)
 print(' A tubuada correspontente é \n {0} * 1 = {1} \n {2} * 2 = {3} \n {4} * 3 = {5} \n {6} * 4 = {7} \n {8} * 5 = {9} \n {10} * 6 = {11} \n {12} * 7 = {13} \n {14} * 8 = {15} \n {16} * 9 = {17} \n {18} * 10 = {19}'.format(t, t*1,t, t*2, t, t*3,t, t*4, t, t*5,t, t*6, t, 
                                                                                                                                                                                                                                 t*7,t, t*8,t, t*9, t, t*10))
+print('-'*12)
