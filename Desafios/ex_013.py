57961136102e2ffebcf530e18291e8a8f52b3246
Ana Caroline Moraes <166078471+Anymoraes5@users.noreply.github.com>
2025-02-13 18:48:57 -0300

    exercicio 13

---
+++ Desafios/ex_013.py
@@ -0,0 +1,5 @@
+#Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento
+salario = float(input('Digite seu salário: '))
+aumento = salario * 0.15
+n_salario = salario + aumento
+print('Seu salário teve ajuste de {} e agora está {}'.format(aumento,n_salario))
