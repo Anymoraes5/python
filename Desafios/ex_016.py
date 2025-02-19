6d52e59ae7c374512683e587e23c6358a5594104
Ana Caroline Moraes <166078471+Anymoraes5@users.noreply.github.com>
2025-02-19 18:28:51 -0300

    [feat] exercicio comparação e if/else

---
+++ Desafios/ex_016.py
@@ -0,0 +1,7 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor < segundo_valor:
+    print(f'Segundo valor {segundo_valor} é maior que {primeiro_valor}')
+elif primeiro_valor > segundo_valor:
+    print(f'Primeiro valor {primeiro_valor} é maior que {segundo_valor}')
