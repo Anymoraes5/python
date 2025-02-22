550a8607ac90df18f81b1fb1636347fe82da0435
Ana Caroline Moraes <166078471+Anymoraes5@users.noreply.github.com>
2025-02-21 21:14:11 -0300

    [FEAT] aula sobre while part 1

---
+++ Testes/aula21_a.py
@@ -0,0 +1,13 @@
+'''
+Repetições
+while (enquanto)
+executa uma ação enquanto uma condição for verdadeira
+'''
+condicao = True
+while condicao:
+    nome = input('Qual o seu nome? ')
+    print(f'Seu nome é {nome}')
+
+    if nome == 'sair':
+        break
+print('Acabou')
