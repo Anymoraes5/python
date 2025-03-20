bb0304fa51882a00031d77ae4f0788cf4a8f8486
Ana Caroline Moraes <166078471+Anymoraes5@users.noreply.github.com>
2025-03-20 20:34:04 -0300

    [FEAT] aula sobre o *args

---
+++ Módulo Intermediário/Aulas/aula_05.py
@@ -0,0 +1,15 @@
+'''
+Retorno das funções
+
+*args (empacotamento e desempacotamento)
+'''
+
+
+def soma(x , y):
+    return x + y
+    print(1+1)
+    
+
+soma1 = soma(1,2)
+soma2 = soma(2,2)
+print(soma1 + soma2)
